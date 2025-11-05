57573307fec0cffbff852b1807bd491a88ce8a4a
bayu_prasetya <bdw.prasetya@gmail.com>
2025-11-05 22:43:02 +0700

    add deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/oxa-coding/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
